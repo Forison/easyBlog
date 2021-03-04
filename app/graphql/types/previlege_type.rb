e9ef6f8169478c6b7914ea5803b379ab6e4eb584
forison <addoboakyeforison@gmail.com>
2021-03-04 17:32:50 -0500

    Connect React for it first render

--- app/graphql/types/previlege_type.rb
+++ app/graphql/types/previlege_type.rb
@@ -5,5 +5,6 @@ module Types
     field :role, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :user Types::UserType, null: false
   end
 end
